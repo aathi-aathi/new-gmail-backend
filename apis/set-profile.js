b9aaa87639bb8f0aac2b69fe83de4129fff7ed04
aathi-aathi <aathivdr2004@gmail.com>
2024-09-11 17:35:32 +0530

    image upload functionality added

--- apis/set-profile.js
+++ apis/set-profile.js
@@ -1,9 +1,27 @@
 import express from 'express'
 import { db } from '../mongodb/mongodb-connect.js'
+import multer from 'multer'
+import path from 'path'
 const setProfile = express.Router()
-setProfile.post('/',async(req,res)=>{
-    const userData = req.body
-    await db.collection('users').updateOne({email:userData.email},{$set:{profile:userData.profile}})
-    res.send({msg:'profile updated successfully'})
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'public/images')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.fieldname+'_'+Date.now()+ path.extname(file.originalname))
+    }
+})
+const upload = multer({
+    storage:storage
+})
+setProfile.post('/:email',upload.single('file'), async(req,res)=>{
+    const param = req.params
+    try {
+        await db.collection('users').updateOne({email:param.email},{$set:{profile:req.file.filename}})
+        res.send({msg:'profile updated successfully'})
+    } catch (error) {
+       res.status(500).send({msg:'not ok'}) 
+    }
+   
 })
 export default setProfile;
